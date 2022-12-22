c77a9146280d550105ffebf07bfd2b4373b969a1
= <alexandressoares383@gmail.com>
2022-12-22 19:46:45 -0300

    feat: applying css

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,20 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.title}>Hello React Native 2022!</Text>
+      <Button title="Clique aqui" />
       <StatusBar style="auto" />
     </View>
+  );
+}
+
+interface ButtonProps {
+  title: string;
+}
+
+function Button(props: ButtonProps) {
+  return (
+    <TouchableOpacity style={styles.button}>
+      <Text>{props.title}</Text>
+    </TouchableOpacity>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#000",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    color: "#fff",
+    fontSize: 22,
+  },
+  button: {
+    backgroundColor: "#fff",
+    color: "#000",
+    marginTop: 15,
+    padding: 10,
+    borderRadius: 6,
   },
 });
